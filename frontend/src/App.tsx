c39a014f2ee89e5a706f6b35ea0cd276136504a0
BRAHIM MAHIOUSSI <37108275+BRAHAIM360@users.noreply.github.com>
2023-02-02 00:02:47 +0100

    change app and index file add scss file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { Route, Routes, Navigate } from "react-router-dom"
+import { AddProduct } from './pages/addProduct';
+import { ListProducts } from './pages/listProducts';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Routes>
+      <Route path="product/new" element={<AddProduct />} />
+      <Route path="product/list" element={<ListProducts />} />
+      <Route
+        path="*"
+        element={<Navigate to="product/list" replace />}
+      />
+    </Routes>
+  )
+
 }
 
 export default App;
