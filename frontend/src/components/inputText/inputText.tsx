734378fa9b4222378b9d71f63fba49701c492e25
BRAHIM MAHIOUSSI <37108275+BRAHAIM360@users.noreply.github.com>
2023-02-03 16:32:03 +0100

    add text  input componenet

---
+++ frontend/src/components/inputText/inputText.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './styles.scss';
+
+interface IInputText {
+    label: string;
+    value: string;
+    onChange: (val: string) => void;
+    type: 'number' | 'text';
+    errorText?: string;
+}
+
+export const InputText = (props: IInputText) => {
+    return (
+        <>
+            <div className='input-text'>
+                <span className='input-text__label'>{props.label}</span>
+                <input type={props.type} className='input-text__input' value={props.value} onChange={(e) => props.onChange(e.target.value)} />
+            </div>
+            {
+                props.errorText && <div className='errorText' >{props.errorText}</div>
+            }
+
+        </>
+    );
+}
+
