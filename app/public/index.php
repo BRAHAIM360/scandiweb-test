95a283422224fc4b5c6bb3855a1c2a4ef2a61d4a
BRAHIM MAHIOUSSI <37108275+BRAHAIM360@users.noreply.github.com>
2023-02-14 11:58:02 +0100

    add get and delete and save routes

--- app/public/index.php
+++ app/public/index.php
@@ -1,24 +1,2 @@
 <?php
-// require __DIR__ . '../vendor/autoload.php';
-echo 'Hello World';
-
-// use src\ProductApi;
-// 
-require ('../vendor/autoload.php');
-use App\Models\Product;
-use App\Database\Database;	
-// use App\Database\Connection;	
-
-$database = new Database();
-$db = $database->connect();
-$product = new Product($db);
-// $product->setName('test');
-echo $product->getName();
-
-// $product = new ProductApi();
-
-// if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-//     $product->handleGetRequest();
-// } elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
-//     $product->handlePostRequest();
-// }
+echo 'Backend is running ';
